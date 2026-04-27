50914bfb40474407ddb1fd3cceca81d9c1aa799c
thetimstarcev <132023910+thetimstarcev@users.noreply.github.com>
2026-04-27 10:05:17 -0400

    Set up application menu structure and create initial Transaction object

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,81 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        mainMenu();
+        System.out.println("Have a great name!");
+    }
+
+    private static void mainMenu() {
+        String prompt = """
+                Welcome to Your Bank Ledger
+                
+                D) Add Deposit
+                P) Make Payment
+                L) Ledger
+                X) Exit
+                
+                Choose an option:
+                """;
+
+        String userInput = scanner.nextLine();
+
+        switch (userInput) {
+            case "D":
+                addDeposit();
+                break;
+            case "P":
+                makePayment();
+                break;
+            case "L":
+                ledger();
+                break;
+            case "X":
+                break;
+            default:
+                System.out.println("Invalid input. Please try again.");
+
+        }
+
+    }
+
+    private static void addDeposit() {
+        System.out.println("--- Add Deposit---");
+        System.out.println("Please enter the transaction description: ");
+        String userInput = scanner.nextLine();
+        System.out.println("Please enter vendor: ");
+        userInput = scanner.nextLine();
+        System.out.println("Please enter the amount:");
+        userInput = scanner.nextLine();
+    }
+
+    private static void makePayment() {
+        System.out.println("--- Make Payment---");
+        System.out.println("Please enter the transaction description: ");
+        String userInput = scanner.nextLine();
+        System.out.println("Please enter vendor: ");
+        userInput = scanner.nextLine();
+        System.out.println("Please enter the amount:");
+        userInput = scanner.nextLine();
+    }
+
+    private static void ledger() {
+        String prompt = """
+                A) All
+                D) Deposits
+                P) Payments
+                R) Reports
+                H) Home
+                
+                Choose an option:
+                """;
+        String userInput = scanner.nextLine();
+        switch (userInput) {
+            case "A":
+        }
+    }
 }
--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -1,4 +1,43 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Transactions {
+    LocalDate date;
+    LocalTime time;
+    String description;
+    String vendor;
+    double amount;
+
+    public Transactions(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    //regiongetters
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+    //endregions
+
 }
